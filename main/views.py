9d1cce08ef6c696dc7b55a95e109f0451dcab00f
charles-ramos <charles.ramos@fatec.sp.gov.br>
2024-09-24 15:34:49 -0300

    adding a test environment variable

--- main/views.py
+++ main/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-import os
 # Create your views here.
 def home(request):
-    test = os.getenv('TEST')
-    print(test)
-    return render(request, test, 'home.html')
+    return render(request, 'home.html')
