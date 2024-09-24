f9f132c31e2d92609b937810b85b4ae5d3556124
charles-ramos <charles.ramos@fatec.sp.gov.br>
2024-09-24 15:18:33 -0300

    adding a test environment variable

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+import os
+from dotenv import load_dotenv
 
 
 class MainConfig(AppConfig):
+    load_dotenv()
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+    test = os.getenv('TEST')
+    print(test)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+import os
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    test = os.getenv('TEST')
+    print(test)
+    return render(request, test, 'home.html')
